c5127e9be1efc7ff1f2c9ccd67dadad892628f52
Syed sameer pasha <128407996+syedsameerpasha@users.noreply.github.com>
2025-12-30 14:30:03 +0530

    Merge master into main

---
+++ demo/src/test/java/com/spring/ai/demo/demo/FirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.ai.demo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
