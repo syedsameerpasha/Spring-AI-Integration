3843a663b923e4f42904cca05c3228156b0a3646
Syed sameer pasha <128407996+syedsameerpasha@users.noreply.github.com>
2026-01-13 11:11:34 +0530

    Added prompt template to avoid hallucinations.

--- demo/src/main/java/com/spring/ai/demo/demo/service/ChatServiceImpl.java
+++ demo/src/main/java/com/spring/ai/demo/demo/service/ChatServiceImpl.java
@@ -1,13 +1,16 @@
 package com.spring.ai.demo.demo.service;
 
 import com.spring.ai.demo.demo.Entity.Tut;
+import jakarta.annotation.Resource;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
+import org.stringtemplate.v4.ST;
 
 import java.util.List;
 import java.util.Map;
@@ -16,7 +19,13 @@ import java.util.Map;
 @Service
 public class ChatServiceImpl implements ChatService {
 
-    private ChatClient chatClient;
+    private final ChatClient chatClient;
+
+    @Value("classpath:/prompts/user-message.st")
+    private Resource userMessage;
+
+    @Value("classpath:/prompts/system-message.st")
+    private Resource systemMessage;
 
     private static final PromptTemplate TUT_PROMPT =
             new PromptTemplate("""
@@ -71,23 +80,32 @@ public class ChatServiceImpl implements ChatService {
      //   ____________________________________________________________________________
 
 
-        var systemPromptTemplate=SystemPromptTemplate.builder()
-                .template("You are a helpful coding assistant. You are an expert in coding.")
-                .build();
-        var systemMessage=systemPromptTemplate.createMessage();
+//        var systemPromptTemplate=SystemPromptTemplate.builder()
+//                .template("You are a helpful coding assistant. You are an expert in coding.")
+//                .build();
+//        var systemMessage=systemPromptTemplate.createMessage();
+//
+//        var userPromptTemplate=PromptTemplate.builder().template("What is {techName}? tell ma also about {techExample}").build();
+//        var userMessage=userPromptTemplate.createMessage(Map.of(
+//                "techName", "Spring",
+//                "techExample", "spring exception"
+//        ));
+//
+//
+//        Prompt prompt = new Prompt(systemMessage,userMessage);
 
-        var userPromptTemplate=PromptTemplate.builder().template("What is {techName}? tell ma also about {techExample}").build();
-        var userMessage=userPromptTemplate.createMessage(Map.of(
-                "techName", "Spring",
-                "techExample", "spring exception"
-        ));
-
-
-        Prompt prompt = new Prompt(systemMessage,userMessage);
-
-        return this.chatClient.prompt(prompt)
+        return this.chatClient
+                .prompt()
+                .system(system ->
+                        system.text(systemMessage.toString())
+                )
+                .user(user ->
+                        user.text(userMessage.toString())
+                                .param("concept", "Python iteration")
+                )
                 .call()
                 .content();
+
 
 
     }
